48d547d15e527b8d4a600141e74099f3ab090ddc
fireb2477-code <fireb2477@gmail.com>
2026-08-11 21:52:38 +0700

    Add files via upload

--- script/c69048.lua
+++ script/c69048.lua
@@ -1,445 +1,162 @@
---Masked HERO Death Law
+-- Masked HERO Death Law
 local s,id=GetID()
-
-s.listed_names={21143940}
-s.listed_series={0x8,0xA008}
-
 function s.initial_effect(c)
-	---------------------------------------------------
-	-- Special Summon procedure
-	-- Banish 1 "Mask Change" from your hand
-	-- or face-down field
-	---------------------------------------------------
-	c:EnableReviveLimit()
-
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_FIELD)
-	e0:SetCode(EFFECT_SPSUMMON_PROC)
-	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e0:SetRange(LOCATION_EXTRA)
-	e0:SetCondition(s.spcon)
-	e0:SetTarget(s.sptg)
-	e0:SetOperation(s.spop)
-	c:RegisterEffect(e0)
-
-	---------------------------------------------------
-	-- You can only control 1 "Masked HERO Death Law"
-	---------------------------------------------------
-	c:SetUniqueOnField(1,0,aux.FilterBoolFunction(Card.IsCode,id),LOCATION_MZONE)
-
-	---------------------------------------------------
-	-- Cards sent to opponent's GY are banished instead
-	---------------------------------------------------
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_TO_GRAVE_REDIRECT)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetTargetRange(0,LOCATION_ONFIELD)
-	e1:SetValue(LOCATION_REMOVED)
-	e1:SetTarget(s.rmtg)
-	c:RegisterEffect(e1)
-
-	---------------------------------------------------
-	-- Gain 200 ATK for each banished card
-	-- from either player's banished zone
-	---------------------------------------------------
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(s.atkval)
-	c:RegisterEffect(e2)
-
-	---------------------------------------------------
-	-- Once while face-up:
-	-- During the End Phase, banish cards from the
-	-- top of your opponent's Deck equal to the
-	-- number of cards in your GY
-	---------------------------------------------------
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_REMOVE)
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_PHASE+PHASE_END)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,id+100)
-	e3:SetCondition(s.epcon)
-	e3:SetTarget(s.eptg)
-	e3:SetOperation(s.epop)
-	c:RegisterEffect(e3)
-
-	---------------------------------------------------
-	-- If this card leaves the field:
-	-- Special Summon 1 DARK "Masked HERO"
-	-- from your Extra Deck, ignoring its
-	-- Summoning conditions.
-	-- Then Set 1 "Mask Change" from your GY
-	-- or banishment.
-	---------------------------------------------------
-	local e4=Effect.CreateEffect(c)
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOFIELD)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e4:SetCode(EVENT_LEAVE_FIELD)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetCondition(s.lvcon)
-	e4:SetTarget(s.lvtg)
-	e4:SetOperation(s.lvop)
-	c:RegisterEffect(e4)
+    c:EnableReviveLimit()
+    
+    -- Giới hạn điều khiển: "You can only control 1 Masked HERO Death Law"
+    c:SetUniqueOnField(1,1,id)
+    
+    -- 1. Must be Special Summoned by banishing 1 "Mask Change" from your hand or face-down field
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_EXTRA)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    e1:SetValue(SUMMON_TYPE_SPECIAL)
+    c:RegisterEffect(e1)
+    
+    -- 3. Any card sent to your opponent's GY is banished instead
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+    e2:SetCode(EFFECT_TO_GRAVE_REDIRECT)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(0,LOCATION_ALL)
+    e2:SetValue(LOCATION_REMOVED)
+    e2:SetTarget(s.rmtarget)
+    c:RegisterEffect(e2)
+    
+    -- 4. Gains 200 ATK for each banished card
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_UPDATE_ATTACK)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetValue(s.atkval)
+    c:RegisterEffect(e3)
+    
+    -- 5. Once while face-up: End Phase banish top of opponent's Deck equal to cards in your GY
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+    e4:SetCategory(CATEGORY_REMOVE)
+    e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_PHASE+PHASE_END)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCondition(s.deckrmcon)
+    e4:SetTarget(s.deckrmtg)
+    e4:SetOperation(s.deckrmop)
+    c:RegisterEffect(e4)
+    
+    -- 6. If this card leaves the field: Special Summon 1 DARK "Masked HERO" from Extra Deck, then Set 1 "Mask Change"
+    local e5=Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id,1))
+    e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+    e5:SetCode(EVENT_LEAVE_FIELD)
+    e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+    e5:SetTarget(s.sptg2)
+    e5:SetOperation(s.spop2)
+    c:RegisterEffect(e5)
 end
 
+s.listed_names={21143940} -- Mask Change
+s.listed_series={0xa008}  -- Masked HERO
 
----------------------------------------------------
--- Special Summon procedure
----------------------------------------------------
-
-function s.mcfilter(c)
-	return c:IsCode(21143940)
-		and c:IsAbleToRemove()
+-- E1: Special Summon Procedure
+function s.spfilter(c)
+    return c:IsCode(21143940) and c:IsAbleToRemoveAsCost()
+        and (c:IsLocation(LOCATION_HAND) or (c:IsLocation(LOCATION_ONFIELD) and c:IsFacedown()))
 end
 
 function s.spcon(e,c)
-	if c==nil then
-		return true
-	end
-
-	if not c:IsLocation(LOCATION_EXTRA) then
-		return false
-	end
-
-	local tp=e:GetHandlerPlayer()
-
-	if Duel.GetLocationCountFromEx(tp,tp,nil)<=0 then
-		return false
-	end
-
-	return Duel.IsExistingMatchingCard(
-		s.mcfilter,
-		tp,
-		LOCATION_HAND+LOCATION_SZONE,
-		0,
-		1,
-		nil
-	)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,nil)
+    return aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0)
 end
 
-
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.GetLocationCountFromEx(tp,tp,nil)>0
-			and Duel.IsExistingMatchingCard(
-				s.mcfilter,
-				tp,
-				LOCATION_HAND+LOCATION_SZONE,
-				0,
-				1,
-				nil
-			)
-	end
-
-	Duel.Hint(
-		HINT_SELECTMSG,
-		tp,
-		HINTMSG_REMOVE
-	)
-
-	local g=Duel.SelectMatchingCard(
-		tp,
-		s.mcfilter,
-		tp,
-		LOCATION_HAND+LOCATION_SZONE,
-		0,
-		1,
-		1,
-		nil
-	)
-
-	if #g>0 then
-		e:SetLabelObject(g:GetFirst())
-	end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+    local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,nil)
+    local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_REMOVE,nil,nil,true)
+    if #g>0 then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+        return true
+    end
+    return false
 end
 
-
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=e:GetLabelObject()
-
-	if not tc then
-		return
-	end
-
-	if not tc:IsRelateToEffect(e) then
-		return
-	end
-
-	if Duel.Remove(
-		tc,
-		POS_FACEUP,
-		REASON_COST
-	)==0 then
-		return
-	end
-
-	Duel.SpecialSummon(
-		c,
-		SUMMON_TYPE_SPECIAL,
-		tp,
-		tp,
-		false,
-		false,
-		POS_FACEUP
-	)
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+    local g=e:GetLabelObject()
+    if not g then return end
+    Duel.Remove(g,POS_FACEUP,REASON_COST)
+    g:DeleteGroup()
 end
 
-
----------------------------------------------------
--- Opponent's cards sent to GY -> banished
----------------------------------------------------
-
-function s.rmtg(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(
-		function(c,tp)
-			return c:IsControler(1-tp)
-		end,
-		1,
-		nil,
-		tp
-	)
+-- E3: Banish redirect filter
+function s.rmtarget(e,c)
+    return c:GetOwner()==1-e:GetHandlerPlayer()
 end
 
-
----------------------------------------------------
--- ATK
----------------------------------------------------
-
+-- E4: ATK gain value
 function s.atkval(e,c)
-	local tp=e:GetHandlerPlayer()
-
-	return (
-		Duel.GetFieldGroupCount(
-			tp,
-			LOCATION_REMOVED,
-			0
-		)
-		+
-		Duel.GetFieldGroupCount(
-			tp,
-			0,
-			LOCATION_REMOVED
-		)
-	)*200
+    return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_REMOVED,LOCATION_REMOVED)*200
 end
 
-
----------------------------------------------------
--- End Phase
----------------------------------------------------
-
-function s.epcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsFaceup()
-		and Duel.IsTurnPlayer(tp)
+-- E5: Banish top deck
+function s.deckrmcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():GetFlagEffect(id)==0
 end
 
-
-function s.eptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local gyct=Duel.GetFieldGroupCount(
-		tp,
-		LOCATION_GRAVE,
-		0
-	)
-
-	local deckct=Duel.GetFieldGroupCount(
-		1-tp,
-		LOCATION_DECK,
-		0
-	)
-
-	local ct=math.min(gyct,deckct)
-
-	if chk==0 then
-		return ct>0
-	end
-
-	Duel.SetOperationInfo(
-		0,
-		CATEGORY_REMOVE,
-		nil,
-		ct,
-		1-tp,
-		LOCATION_DECK
-	)
-
-	e:SetLabel(ct)
+function s.deckrmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local ct=Duel.GetFieldGroupCount(tp,LOCATION_GRAVE,0)
+    if chk==0 then return ct>0 and Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>=ct end
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,ct,1-tp,LOCATION_DECK)
 end
 
-
-function s.epop(e,tp,eg,ep,ev,re,r,rp)
-	local ct=e:GetLabel()
-
-	if not ct or ct<=0 then
-		return
-	end
-
-	if Duel.GetFieldGroupCount(
-		1-tp,
-		LOCATION_DECK,
-		0
-	)<=0 then
-		return
-	end
-
-	Duel.DisableShuffleCheck()
-
-	local g=Duel.GetDecktopGroup(
-		1-tp,
-		ct
-	)
-
-	if #g>0 then
-		Duel.Remove(
-			g,
-			POS_FACEUP,
-			REASON_EFFECT
-		)
-	end
+function s.deckrmop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) and c:IsFaceup() then
+        c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
+    end
+    local ct=Duel.GetFieldGroupCount(tp,LOCATION_GRAVE,0)
+    if ct>0 and Duel.GetFieldGroupCount(tp,0,LOCATION_DECK)>=ct then
+        local g=Duel.GetDecktopGroup(1-tp,ct)
+        if #g>0 then
+            Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+        end
+    end
 end
 
-
----------------------------------------------------
--- Leave field
----------------------------------------------------
-
-function s.lvcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-
-	return c:IsPreviousLocation(LOCATION_MZONE)
+-- E6: Leave field effect
+function s.spfilter2(c,e,tp)
+    return c:IsSetCard(0xa008) and c:IsAttribute(ATTRIBUTE_DARK)
+        and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 
-
----------------------------------------------------
--- DARK Masked HERO
----------------------------------------------------
-
-function s.spfilter(c,e,tp)
-	return c:IsSetCard(0xA008)
-		and c:IsAttribute(ATTRIBUTE_DARK)
-		and c:IsType(TYPE_FUSION)
-		and c:IsCanBeSpecialSummoned(
-			e,
-			SUMMON_TYPE_SPECIAL,
-			tp,
-			true,
-			true
-		)
+function s.setfilter(c)
+    return c:IsCode(21143940) and c:IsSSetable()
 end
 
-
-function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.GetLocationCountFromEx(tp,tp,nil)>0
-			and Duel.IsExistingMatchingCard(
-				s.spfilter,
-				tp,
-				LOCATION_EXTRA,
-				0,
-				1,
-				nil,
-				e,
-				tp
-			)
-	end
-
-	Duel.Hint(
-		HINT_SELECTMSG,
-		tp,
-		HINTMSG_SPSUMMON
-	)
-
-	local g=Duel.SelectMatchingCard(
-		tp,
-		s.spfilter,
-		tp,
-		LOCATION_EXTRA,
-		0,
-		1,
-		1,
-		nil,
-		e,
-		tp
-	)
-
-	Duel.SetTargetCard(g)
-
-	Duel.SetOperationInfo(
-		0,
-		CATEGORY_SPECIAL_SUMMON,
-		g,
-		1,
-		0,
-		0
-	)
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
-
----------------------------------------------------
--- Mask Change to Set
----------------------------------------------------
-
-function s.mcsetfilter(c)
-	return c:IsCode(21143940)
-		and c:IsSSetable()
-end
-
-
-function s.lvop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-
-	if not tc
-		or not tc:IsRelateToEffect(e)
-	then
-		return
-	end
-
-	if Duel.SpecialSummon(
-		tc,
-		SUMMON_TYPE_SPECIAL,
-		tp,
-		tp,
-		true,
-		true,
-		POS_FACEUP
-	)<=0 then
-		return
-	end
-
-	local g=Duel.GetMatchingGroup(
-		s.mcsetfilter,
-		tp,
-		LOCATION_GRAVE+LOCATION_REMOVED,
-		0,
-		nil
-	)
-
-	if #g==0 then
-		return
-	end
-
-	Duel.Hint(
-		HINT_SELECTMSG,
-		tp,
-		HINTMSG_TOFIELD
-	)
-
-	local mc=g:Select(
-		tp,
-		1,
-		1,
-		nil
-	):GetFirst()
-
-	if mc then
-		Duel.SSet(
-			tp,
-			mc
-		)
-	end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCountFromEx(tp)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+    if #g>0 and Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)>0 then
+        local sg=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil)
+        if #sg>0 and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
+            Duel.BreakEffect()
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+            local setg=sg:Select(tp,1,1,nil)
+            Duel.SSet(tp,setg)
+        end
+    end
 end
