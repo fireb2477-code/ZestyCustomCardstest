de1b5fedb823a99b0a194ae6668543c53d661e2e
fireb2477-code <fireb2477@gmail.com>
2026-08-14 20:29:27 +0700

    Add files via upload

---
+++ script/c23481.lua
@@ -0,0 +1,141 @@
+-- Slime, The First Choker
+local s,id=GetID()
+function s.initial_effect(c)
+    -- 1. Special Summon from Hand and Search
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_SEARCH+CATEGORY_TOHAND)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.spcon1)
+    e1:SetTarget(s.sptg1)
+    e1:SetOperation(s.spop1)
+    c:RegisterEffect(e1)
+
+    -- 2. Tribute to Special Summon and apply Protection
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,id+1)
+    e2:SetCost(s.spcost2)
+    e2:SetTarget(s.sptg2)
+    e2:SetOperation(s.spop2)
+    c:RegisterEffect(e2)
+
+    -- 3. Banish to inflict Damage
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,2))
+    e3:SetCategory(CATEGORY_DAMAGE)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetCountLimit(1,id+2)
+    e3:SetCost(aux.bfgcost)
+    e3:SetTarget(s.damtg)
+    e3:SetOperation(s.damop)
+    c:RegisterEffect(e3)
+end
+
+-- Khai báo liên kết với Kashtira (0x189) để engine hiển thị gợi ý bài tốt hơn
+s.listed_series={0x189}
+
+-- E1 Logic: SpSummon & Search
+function s.cfilter(c)
+    return c:IsFaceup() and c:IsSetCard(0x189)
+end
+function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
+        or Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.thfilter(c)
+    return c:IsSetCard(0x189) and c:IsAbleToHand()
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+        and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+        if #g>0 then
+            Duel.SendtoHand(g,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,g)
+        end
+    end
+end
+
+-- E2 Logic: Tribute to SpSummon & Protect
+function s.spcost2(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():IsReleasable() end
+    Duel.Release(e:GetHandler(),REASON_COST)
+end
+function s.spfilter2(c,e,tp)
+    return c:IsSetCard(0x189) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        -- Kể cả khi full bàn, hy sinh lá này xong vẫn sẽ trống 1 ô để Summon
+        return Duel.GetMZoneCount(tp,e:GetHandler())>0
+            and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter2),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+    if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+        local c=e:GetHandler()
+        
+        -- Hiệu ứng chặn đối thủ Respond
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+        e1:SetCode(EVENT_CHAINING)
+        e1:SetOperation(s.chainop)
+        -- Tồn tại đến hết lượt của đối thủ (RESET_OPPO_TURN, đếm 1 lần)
+        e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN,1)
+        Duel.RegisterEffect(e1,tp)
+        
+        -- Dấu hiệu nhận biết cho người chơi (Client Hint)
+        local e2=Effect.CreateEffect(c)
+        e2:SetType(EFFECT_TYPE_FIELD)
+        e2:SetCode(EFFECT_FLAG_CLIENT_HINT)
+        e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+        e2:SetTargetRange(1,0)
+        e2:SetDescription(aux.Stringid(id,3))
+        e2:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN,1)
+        Duel.RegisterEffect(e2,tp)
+    end
+end
+function s.chainop(e,tp,eg,ep,ev,re,r,rp)
+    local rc=re:GetHandler()
+    -- Nếu người kích hoạt là mình (tp), bài kích hoạt thuộc tính Kashtira, và là Monster Effect
+    if ep==tp and rc:IsSetCard(0x189) and re:IsActiveType(TYPE_MONSTER) then
+        Duel.SetChainLimit(s.chainlm)
+    end
+end
+function s.chainlm(e,rp,tp)
+    return tp==rp -- Chỉ người chơi hiện tại mới được chain tiếp, đối thủ thì không
+end
+
+-- E3 Logic: Banish to Burn
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_REMOVED,LOCATION_REMOVED)>0 end
+    local ct=Duel.GetFieldGroupCount(tp,LOCATION_REMOVED,LOCATION_REMOVED)
+    Duel.SetTargetPlayer(1-tp)
+    Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*300)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+    local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+    local ct=Duel.GetFieldGroupCount(tp,LOCATION_REMOVED,LOCATION_REMOVED)
+    if ct>0 then
+        Duel.Damage(p,ct*300,REASON_EFFECT)
+    end
+end
